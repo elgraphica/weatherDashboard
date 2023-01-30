ac71d93ec281e7306e4ee5fda93ed0521cf4bbfb
elgraphica <elferenczi@outlook.com>
2023-01-30 00:46:49 +0000

    input to local storage and history list to local storage complete

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-// 1. style HTML
-// 2. trigger geocoding api when searching to find LAT and LON
-// 3. call 5 day weather API after we have city LAT and LON value
-// 4. put the response in HTML
-// 5. put todays weather in container for todays weather
-// 6. put 5 day focast weather in container for the forecast
-// 7. add the input to local storage
-// 8. populate history list from local storage when page loads
 
-// .val() => get if no parameter passed e.g val()
-//        => set if 1 parameter passed e.g val('input')
-
+const history = JSON.parse(localStorage.getItem('history')) || [];
 const apiKey = '996513822471b1a94113e31bc557ef81';
 
 const userInput = $('#search-input').val();
@@ -19,7 +9,11 @@ $('#search-form').on('submit', function(event) {
 
     const userInput = $('#search-input').val();
 
-    const queryUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=&appid=' + apiKey;
+    const queryUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=5&appid=' + apiKey;
+
+    history.push(userInput);
+    
+    localStorage.setItem('history', JSON.stringify(history));
 
     $.ajax({ url: queryUrl })
     .then(function(response) {
@@ -30,13 +24,34 @@ $('#search-form').on('submit', function(event) {
 
     console.log(lat, lon);
 
-    const weatherQueryUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+    const weatherQueryUrl = 'http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
 
     $.ajax({ url: weatherQueryUrl })
     .then(function(weatherResponse) {
+        //Icon URL --> "http://openweathermap.org/img/w/" + iconcode + ".png"
         console.log(weatherResponse);
+
+        const weatherList = weatherResponse.list;
+
+        const today = weatherList[0]
+        const todayTimeStamp = today.dt;
+        console.log(today);
+
+        for (let i = 1; i < weatherList.length; i += 8) {
+
+            const weather = weatherList[i];
+            const timestamp = weather.dt;
+            console.log(weather);
+
+        }
     });
 
     });
 
 });
+
+//ToDo
+// 1 prepend the value to the list container
+// 2 put today's weather in container---> today's weather
+// 3 put 5 days forecast weather in container for the forecast
+// 4 style HTML
