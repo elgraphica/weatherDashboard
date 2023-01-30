55d640e52272eccb9e3879e859850e1f9760c9b4
elgraphica <elferenczi@outlook.com>
2023-01-30 16:11:19 +0000

    displaying todays weather on page complete

--- script.js
+++ script.js
@@ -4,54 +4,71 @@ const apiKey = '996513822471b1a94113e31bc557ef81';
 
 const userInput = $('#search-input').val();
 
-$('#search-form').on('submit', function(event) {
-    event.preventDefault();
-
-    const userInput = $('#search-input').val();
-
-    const queryUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=5&appid=' + apiKey;
-
-    history.push(userInput);
-    
-    localStorage.setItem('history', JSON.stringify(history));
-
-    $.ajax({ url: queryUrl })
-    .then(function(response) {
-        console.log(response);
-
-    const lat = response[0].lat;
-    const lon = response[0].lon;
-
-    console.log(lat, lon);
-
-    const weatherQueryUrl = 'http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
-
-    $.ajax({ url: weatherQueryUrl })
-    .then(function(weatherResponse) {
-        //Icon URL --> "http://openweathermap.org/img/w/" + iconcode + ".png"
-        console.log(weatherResponse);
-
-        const weatherList = weatherResponse.list;
-
-        const today = weatherList[0]
-        const todayTimeStamp = today.dt;
-        console.log(today);
-
-        for (let i = 1; i < weatherList.length; i += 8) {
-
-            const weather = weatherList[i];
-            const timestamp = weather.dt;
-            console.log(weather);
-
-        }
-    });
-
-    });
-
-});
-
 //ToDo
 // 1 prepend the value to the list container
 // 2 put today's weather in container---> today's weather
 // 3 put 5 days forecast weather in container for the forecast
 // 4 style HTML
+
+$('#search-form').on('submit', function(event) {
+    event.preventDefault();
+  
+    const userInput = $('#search-input').val();
+  
+    const queryUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=5&appid=' + apiKey;
+  
+    history.push(userInput);
+    
+    localStorage.setItem('history', JSON.stringify(history));
+  
+    $.ajax({ url: queryUrl })
+    .then(function(response) {
+      console.log(response);
+  
+      const lat = response[0].lat;
+      const lon = response[0].lon;
+  
+      console.log(lat, lon);
+  
+      const weatherQueryUrl = 'http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+  
+      $.ajax({ url: weatherQueryUrl })
+      .then(function(weatherResponse) {
+        //Icon URL --> "http://openweathermap.org/img/w/" + iconcode + ".png"
+        console.log(weatherResponse);
+  
+        const weatherList = weatherResponse.list;
+  
+        const today = weatherList[0];
+  
+        const iconCode = today.weather[0].icon;
+        const cityName = weatherResponse.city.name;
+        const currentDate = new Date();
+        const formattedDate = currentDate.toLocaleDateString();
+        const iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        const temperature = today.main.temp;
+        const weatherDescription = today.weather[0].description;
+        const windSpeed = today.wind.speed;
+        const humidity = today.main.humidity;
+  
+        $('#today').html(`
+          <p>City: ${cityName}</p>
+          <p>Date: ${formattedDate}</p>
+          <img src="${iconUrl}" alt="Weather Icon">
+          <p>${weatherDescription}</p>
+          <p>Temperature: ${temperature} &#8451;</p>
+          <p>Wind Speed: ${windSpeed} m/s</p>
+          <p>Humidity: ${humidity}%</p>
+        `);
+  
+        const todayTimeStamp = today.dt;
+        console.log(today);
+  
+        for (let i = 1; i < weatherList.length; i += 8) {
+          const weather = weatherList[i];
+          const timestamp = weather.dt;
+          console.log(weather);
+        }
+      });
+    });
+  });
